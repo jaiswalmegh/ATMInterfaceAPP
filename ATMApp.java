09e286571935a324b7e3f05234d228c6b7ae391f
jaiswalmegh <jaiswalmegh42@gmail.com>
2025-05-29 01:50:43 +0530

    Delete ATMApp.java

--- ATMApp.java
+++
@@ -1,7 +0,0 @@
-import ui.ATMInterface;
-
-public class ATMApp {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> new ATMInterface().setVisible(true));
-    }
-}
