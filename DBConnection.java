b65f8cb21f116605e589cc4260ce0d1c524aee20
jaiswalmegh <jaiswalmegh42@gmail.com>
2025-05-29 22:20:15 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/myatm_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "0210";
+    private static final String PASSWORD = "your sql password";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
